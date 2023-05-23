0c982904411704446ac5bb496d4028183e48bcf5
Marcos Fellipe Guimarães <marcosfgui2006@gmail.com>
2023-05-22 22:37:23 -0300

    Initial Commit

---
+++ android/app/src/main/java/com/example/dicionarioapp/app_dicio/MainActivity.java
@@ -0,0 +1,6 @@
+package com.example.dicionarioapp.app_dicio;
+
+import io.flutter.embedding.android.FlutterActivity;
+
+public class MainActivity extends FlutterActivity {
+}
---
+++ lib/main.dart
@@ -0,0 +1,9 @@
+import 'package:flutter/material.dart';
+
+import 'View/Home.dart';
+
+void main() {
+  runApp(MaterialApp(
+    home: Home()
+  ));
+}
